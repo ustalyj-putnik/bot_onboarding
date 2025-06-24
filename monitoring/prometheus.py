af75b5b65d2bef56a7a39f96d637a7524dea5920
Nikita Novikov <144739976+ustalyj-putnik@users.noreply.github.com>
2025-06-24 23:29:08 +0300

    Delete monitoring directory

--- monitoring/prometheus.py
+++
@@ -1,4 +0,0 @@
-from prometheus_client import Histogram, Counter
-RAG_LATENCY = Histogram("rag_latency_seconds", "RAG end-to-end latency")
-ANS_GOOD    = Counter("answer_good_total", "Positive ratings")
-ANS_BAD     = Counter("answer_bad_total", "Negative ratings")
